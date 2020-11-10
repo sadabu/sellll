7261e6da1361dad25ea0c9345cde534871680d58
Abdulla <abdullohsadullayev@gmail.com>
2020-11-10 18:34:00 +0500

    mm

---
+++ app/src/main/java/com/example/sellll/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.sellll
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    internal  val  TIME_OUT=2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Handler().postDelayed(
+                {
+                    startActivity(Intent(this@MainActivity,DemooActivity::class.java))
+                    finish()
+                },TIME_OUT.toLong()
+        )
+
+    }
+
+}
